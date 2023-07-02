3f0596f222ef718bc78f2853a7560449af7ecfcc
Stephen Wong <wongstephenky@gmail.com>
2023-07-02 12:09:57 -0700

    added cors

--- src/api/axiosApi.js
+++ src/api/axiosApi.js
@@ -56,8 +56,7 @@ export const toolCreateAxios = async (formData, token) => {
       headers: {
         Authorization: `Bearer ${token}`,
         "Content-Type": "multipart/form-data",
-        "Access-Control-Allow-Headers":
-          "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept,Authorization",
+        "Access-Control-Allow-Headers": "*",
         "Access-Control-Allow-Origin": "*",
       },
       data: formData,
