dc44725a42cbacaa075aed5524824a1d0194df2e
Stephen Wong <wongstephenky@gmail.com>
2023-07-02 11:30:08 -0700

    updated add image accept input

--- src/components/ItemAdd.js
+++ src/components/ItemAdd.js
@@ -136,7 +136,7 @@ export const ItemAdd = () => {
               name="userImage"
               id="userImage"
               onChange={handleUserImage}
-              accept="image/png, image/jpeg, image/jpg, image/gif"
+              accept="image/*"
             />
 
             {/* image preview */}
