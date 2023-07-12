1be4f6c4f03ae9cf42384b25fc82f8724a852c2c
Stephen Wong <wongstephenky@gmail.com>
2023-07-11 22:27:08 -0700

    updated padding

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div className="min-h-screen sm:py-10 bg-theme-green">
-      <div className="max-w-2xl p-4 pb-32 m-auto bg-slate-50 sm:rounded-xl">
+      <div className="max-w-2xl p-1 pb-32 m-auto bg-slate-50 sm:rounded-xl">
         <Outlet />
       </div>
     </div>
--- src/components/test/LoginRegisterLink.test.js
+++ src/components/test/LoginRegisterLink.test.js
@@ -1,15 +1,91 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import { LoginRegisterLink } from "../presentational/LoginRegisterLink";
-import { BrowserRouter as Router } from "react-router-dom";
-describe("LoginRegisterLink", () => {
-  it("register text links to register component", () => {
-    render(
-      <Router>
-        <LoginRegisterLink />
-      </Router>
-    );
-    const registerLink = screen.getByText(/register/i);
-    fireEvent.click(registerLink);
-    expect(registerLink).toBeEnabled(); //need to fix
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { Register } from "./Register";
+
+describe("Register", () => {
+  test("renders the register form", () => {
+    render(<Register />);
+
+    // Assert that the title is rendered
+    expect(screen.getByTestId("title")).toBeInTheDocument();
+
+    // Assert that the email input is rendered
+    expect(screen.getByLabelText("email")).toBeInTheDocument();
+
+    // Assert that the password input is rendered
+    expect(screen.getByLabelText("password")).toBeInTheDocument();
+
+    // Assert that the confirm password input is rendered
+    expect(screen.getByLabelText("confirm password")).toBeInTheDocument();
+
+    // Assert that the register button is rendered
+    expect(screen.getByLabelText("register")).toBeInTheDocument();
+
+    // Assert that the cancel button is rendered
+    expect(screen.getByLabelText("cancel")).toBeInTheDocument();
+  });
+
+  test("submits the form successfully", async () => {
+    // Mock the userRegisterAxios function
+    const userRegisterAxiosMock = jest.fn().mockResolvedValue({ status: 201 });
+    jest.mock("../api/axiosApi", () => ({
+      userRegisterAxios: userRegisterAxiosMock,
+    }));
+
+    render(<Register />);
+
+    // Fill in the form inputs
+    fireEvent.change(screen.getByLabelText("email"), {
+      target: { value: "test@example.com" },
+    });
+    fireEvent.change(screen.getByLabelText("password"), {
+      target: { value: "password123" },
+    });
+    fireEvent.change(screen.getByLabelText("confirm password"), {
+      target: { value: "password123" },
+    });
+
+    // Submit the form
+    fireEvent.click(screen.getByLabelText("register"));
+
+    // Assert that the userRegisterAxios function was called with the correct form values
+    await waitFor(() => {
+      expect(userRegisterAxiosMock).toHaveBeenCalledWith({
+        email: "test@example.com",
+        password: "password123",
+        confirmPassword: "password123",
+      });
+    });
+
+    // Assert that the navigation occurred
+    // You might need to mock the useNavigate hook for this test
+    // and check that the navigate function was called with the correct path
+  });
+
+  test("displays an error when passwords do not match", async () => {
+    render(<Register />);
+
+    // Fill in the form inputs
+    fireEvent.change(screen.getByLabelText("email"), {
+      target: { value: "test@example.com" },
+    });
+    fireEvent.change(screen.getByLabelText("password"), {
+      target: { value: "password123" },
+    });
+    fireEvent.change(screen.getByLabelText("confirm password"), {
+      target: { value: "differentpassword" },
+    });
+
+    // Submit the form
+    fireEvent.click(screen.getByLabelText("register"));
+
+    // Assert that the error message is displayed
+    await waitFor(() => {
+      expect(
+        screen.getByText(
+          "Passwords do not Match. Please enter your password again."
+        )
+      ).toBeInTheDocument();
+    });
   });
 });
